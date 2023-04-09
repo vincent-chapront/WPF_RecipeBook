b7cfa699ba6a79b49d3fb64d76a57e2c62f9a51c
vincent-chapront <vincent.chapront@gmail.com>
2023-04-09 20:48:29 +0200

    CH09_06.SimpleAnimation

---
+++ CH09 Graphics and Animation/CH09_06.SimpleAnimation/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CH09_06.SimpleAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var animation = new DoubleAnimation(360, TimeSpan.FromSeconds(2), FillBehavior.Stop);
+            Storyboard.SetTarget(animation, rot1);
+            rot1.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+    }
+}
